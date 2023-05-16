ddc7330ccd5e1c2b3d85935ece0f399089488bdf
Hugo Vinícius <ugopessoa@gmail.com>
2023-05-16 15:48:23 -0300

    projeto quase pronto

---
+++ quiz/src/components/Option.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import "./option.css"
+export const Option = ({option, selectOption, answer}) => {
+    const [quizState, dispatch] = useContext(QuizContext);
+  
+    return (
+        <div className="option" onClick={() => selectOption()}>
+            <p>{option}</p>
+        </div>
+    )
+}
+
+export default Option;
